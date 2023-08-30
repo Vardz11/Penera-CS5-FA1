f9801bea34629c0c7220e504e4caa72a709c5da7
Pisay-CRC Student <epenera@crc.pshs.edu.ph>
2023-08-30 15:08:37 +0800

    Initial Commit

---
+++ main.py
@@ -0,0 +1,22 @@
+name = "Como"
+age = 16
+hobby = "studying"
+address = "Pisay CRC"
+core = "Physics"
+coreLevel = 2
+elective = "ComSci"
+favoriteFood = "Caviar"
+section = "Mars"
+gradeLevel = 11
+
+userName = input("Hi! I'm " + name + " from " + address + ". I like " + hobby + " What's your name?")
+input("Hi " + userName + "! Nice to meet you. How old are you?")
+print("Nice! I'm ", age,  " years old!")
+print("I'm Grade ", gradeLevel, " from section " + section)
+print("My elective is Level ", coreLevel, " " + core)
+userFavFood = input("What is your favorite food?")
+print(userFavFood + ", huh? Well, I personally like " + favoriteFood)
+
+
+
+
